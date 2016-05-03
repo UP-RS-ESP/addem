e42325d0468584bd3fbd2defb603794a058d551c
Bedartha <goswami@uni-potsdam.de>
2016-05-03 15:55:22 +0200

    Take care of ImportError for init file

--- addem/__init__.py
+++ addem/__init__.py
@@ -1,9 +1,12 @@
 #! /usr/bin/env python
 # __INIT__.PY
 
-from addem import flows
-from addem import distributions
-from addem import sinks
+try:
+    from addem import flows
+    from addem import distributions
+    from addem import sinks
+except ImportError:
+    pass
 
 __all__ = ["flows", "distributions", "sinks"]
 __version__ = '0.0.1.dev1'
